92b5e1c2a0d798a9932cea70340cecf39c36f8b6
jesus-son-coder <rvck2007@gmail.com>
2019-06-06 13:16:55 +0200

    Création du FormType "ImageType"

---
+++ src/AppBundle/Form/ImageType.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('url', 'text')
+            ->add('alt', 'text')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Image'
+        ]);
+    }
+
+    public function getName()
+    {
+        return 'app_bundle_imagetype';
+    }
+}
