e28215fd3d770eb442dcbadcde7ef6ca88250481
jesus-son-coder <rvck2007@gmail.com>
2019-06-07 23:51:40 +0200

    Utiliser un QueryBuilder dynamique avec une variable dans un FormType

---
+++ src/AppBundle/Form/CommentaireWithVariablbeType.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace AppBundle\Form;
+
+use AppBundle\Form\ImageType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentaireWithVariableType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $varialble = 1;
+
+        $builder
+            ->add('contenu', 'textarea')
+            ->add('auteur', 'text')
+
+            ->add('article', 'entity', [
+                'class' => 'AppBundle:Article',
+                'property' => 'titre',
+
+                'query_builder' => function(\AppBundle\Repository\ArticleRepository $r) use($varialble) {
+                    return $r->getSelectList($varialble);
+                }
+            ])
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Commentaire',
+        ]);
+    }
+
+    public function getName()
+    {
+        return 'app_bundle_articletype';
+    }
+}
