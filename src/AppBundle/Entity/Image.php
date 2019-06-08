fed3f02bdabd4c1b0a576ff86de1210360d21655
jesus-son-coder <rvck2007@gmail.com>
2019-06-08 17:45:10 +0200

    Automatiser le traitement des Upload et Suppression des Images grâce aux Evènements :
    Quels évènements utilliser ?
    Dans notre cas, il y a 4 actions à exécuter :
    
    - Avant l'enregistrement effectif en Base de données :
    Il faut remplir les attributs "$url" et "$alt", avec les bonnes valeurs suivant le fichier envoyé. On doit impérativement le faire avant l'enregistrement, pour qu'il puissent enregitrés eux-mêmes en base de données.
    Pour cette action, il faut utiliser les évènements :
    -> "PrePersist"
    -> "PreUpdate"
    
    - Juste après l'enregistrement :
    Il faut déplacer effectivement le fichier envoyé.
    On ne le fait pas avant, car l'enregistrement peut échouer. En cas d'échec, il ne faudrait pass se retrouver avec un fichier orphelin sur notre disque. On attend donc que l'enregistrement se fasse avant de déplacer le fichier.
    Pour cette action, il faut utiliser les évènements :
    -> "PostPersist"
    -> "PostUpdate"
    
    - Juste avant la suppression :
    Il faut sauvegarder le nom du fichier dans un attribut non persisté: "$filename".
    En effet, le nom du fichier dépendant de l'id, on y aura plus accès en "PostRemove", on est donc obligé de sauvegarder en "PreRemove".
    Pour cette action, il faut utiliser les évènements :
    -> "PreRemove"
    
    - Juste après la suppression :
    Il faut supprimer le fichier qui était associé à l'entité.
    Enore une fois, on ne le fait pas avant la suppression, car si l'entité n'est ua final pas supprimée, on aurait alors une entité sans fichier.
    Pour cette action, il faut utiliser les évènements :
    -> "PostRemove".

--- src/AppBundle/Entity/Image.php
+++ src/AppBundle/Entity/Image.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
  * Class Image
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="AppBundle\Repository\ImageRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Image
 {
@@ -26,6 +27,8 @@ class Image
      * @var string url
      *
      * @ORM\Column(name="url", type="string", length=255)
+     *
+     * Désormais, l'attribut "url" n'est constitué que de l'extension du fichier, et nom plus de son chemin complet :
      */
     private $url;
 
@@ -37,6 +40,11 @@ class Image
      * Mais c'est bien cet attribut qui servira pour le formulaire...
      */
     private $file;
+
+    /*
+     * On ajoute cet attribut pour y stocker le nom du fichier temporairement
+     */
+    private $tempFilename;
 
     /**
      * @var string $alt
@@ -103,6 +111,11 @@ class Image
         return $this->alt;
     }
 
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
     public function upload()
     {
         // Si jamais il n'y a pas de fichier (champ facultatif) :
@@ -110,18 +123,29 @@ class Image
             return;
         }
 
-        // On garde le nom original du fichier de l'internaute :
-        $name = $this->file->getClientOriginalName();
+        // si on avait un ancien fichier, on le supprime :
+        if (null == $this->tempFilename) {
+            $oldFile = $this->getUploadRootDir() . '/' . $this->id . '.' . $this->tempFilename;
+
+            if (file_exists($oldFile)) {
+                unlink($oldFile);
+            }
+        }
 
         // On déplace le fichier envoyé dans le répertoire de notre choix :
-        $this->file->move($this->getUploadRootDir(), $name);
+        $this->file->move(
+            // Le répertoire de destination :
+            $this->getUploadRootDir(),
 
-        // On sauvegarde le nom de fichier dans notre attribut $url :
-        $this->url = $name;
-
-        // On crée également le futur attribut alt de notre balise <img> :
-        $this->alt = $name;
+            // Le nom du fichier à créer, ici "id.extension" :
+            $this->id . '.' . $this->url
+        );
     }
+
+
+    /**
+     * @return string
+     */
 
     public function getUploadDir()
     {
@@ -149,11 +173,71 @@ class Image
 
     /**
      * @param mixed $file
+     *
+     * On modifie le Setter de "File", pour prendre en compte l'upload d'un nouveau fichier lorsqu'il en existe déjà un autre.
      */
     public function setFile($file)
     {
         $this->file = $file;
+
+        // On vérifie si on avait déjà un fichier pour cette entité :
+        if (null !== $this->url) {
+            // On sauvegarde l'extension du fichier pour le supprimer plus tard :
+            $this->tempFilename = $this->url;
+
+            // On réinitialise les valeurs des attributs url et alt :
+            $this->url = null;
+            $this->alt = null;
+        }
     }
 
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        // Si jamais il n'y a pas de fichier (champ facultatif) :
+        if (null === $this->file) {
+            return;
+        }
+
+        /*
+         * Le nom du fichier est son id, on doit juste stocker également son extension.
+         * Pour faire propre, on devrait renommer cet attribut en "extension", plutôt que "url" :
+         */
+        $this->url = $this->file->guessExtension();
+
+        /*
+         * Et on génère l'attribut "alt" de la balise <img>, à la valeur du nom du fichier sur le PC de l'internaute :
+         */
+        $this->alt = $this->file->getClientOriginalName();
+
+    }
+
+
+
+    /**
+     * @ORM\PreRemove()
+     */
+    public function preRemoveUpload()
+    {
+        // On sauvegarde temporairement le nom du fichier, car il dépend de l'id :
+        $this->tempFilename = $this->getUploadRootDir() . '/' . $this->id . '.' . $this->url;
+    }
+
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        // En PostRemove, on n'a pas accès à l'id, on utilise notre nom sauvegardé :
+        if (file_exists($this->tempFilename)) {
+            // On supprime le fichier :
+            unlink($this->tempFilename);
+        }
+    }
 
 }
