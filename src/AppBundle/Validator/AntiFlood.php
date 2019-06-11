bef22b02de44ac497760f904a056673e8ee830fc
jesus-son-coder <rvck2007@gmail.com>
2019-06-11 21:55:34 +0200

    Modification de la Contrainte et du Validateur

--- src/AppBundle/Validator/AntiFlood.php
+++ src/AppBundle/Validator/AntiFlood.php
@@ -17,4 +17,9 @@ use Symfony\Component\Validator\Constraint;
 class AntiFlood extends Constraint
 {
     public $message = "Vous avez déjà posté un message il y a moins de 15 secondes, merci d'attendre un peu.";
+
+    public function validatedBy()
+    {
+        return 'blog_antiflood';
+    }
 }
--- src/AppBundle/Validator/AntiFloodValidator.php
+++ src/AppBundle/Validator/AntiFloodValidator.php
@@ -6,6 +6,9 @@ namespace AppBundle\Validator;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * Class AntiFloodValidator
  * c'est la classe Validateur.
@@ -13,6 +16,18 @@ use Symfony\Component\Validator\ConstraintValidator;
  */
 class AntiFloodValidator extends ConstraintValidator
 {
+    private $request;
+    private $em;
+
+    /*
+     * Les arguments déclarés dans la définition du service arrivent au constructeur. On doit les enregitrer dans l'objet pour pouvoir s'en resservir dans la méthode "validate()" :
+     */
+    public function __construct(Request $request, EntityManager $em)
+    {
+        $this->request = $request;
+        $this->em = $em;
+
+    }
 
     /**
      * Checks if the passed value is valid.
@@ -22,12 +37,20 @@ class AntiFloodValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint)
     {
-        // Pour l'instant, on considère comme "flood" tout message de moins de 3 caractères :
-        if (strlen($value) < 3) {
+        // On récupère l'ID de celui qui poste :
+        $ip = $this->request->server->get('REMOTE_ADDR');
+
+        // On vérifie si cette IP a déjà posté un message il y a moins de 15 secondes..
+        // Bien entedu, il faudrait écrire cette méthode isFlood => c'est pour l'exemple !!!
+        $isFlood = $this->em->getRepository('AppBundle:Commentaire')->isFlood($ip, 15);
+
+        // On considère comme "flood" tout message de moins de 3 caractères :
+        if (strlen($value) < 3 && $isFlood) {
             // C'est cette ligne qui déclenche l'erreur pour le formulaire, avec en argument le message :
             $this->context->addViolation($constraint->message);
-            // ou :
-            // $this->context->addViolation($constraint->message, ['%string%' => $value]);
+
         }
+        /* Et voilà, nous venons de faire une contrainte qui s'utilise aussi facilement qu'une annotation, et qui pourtant fait un gros travail en allant chercher dans la base de données si l'IP courante envoie trop de messages.
+        Un peu de travail à la création de la contrainte, mais son utilisation est un jeu d'enfant à présent ! */
     }
 }
